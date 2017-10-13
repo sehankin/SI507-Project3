e78b6a7ae66c938040ccfda337225d44f2b81af9
Saul Hankin <sehankin@umich.edu>
2017-10-13 17:20:23 -0400

    Changed part 1 back to use .find() and .find_all() after all

--- si507f17_project3_code.py
+++ si507f17_project3_code.py
@@ -2,42 +2,6 @@ from bs4 import BeautifulSoup
 import unittest
 import requests
 
-# Instr note: the outline comments will stay as suggestions,
-# otherwise it's too difficult.
-# Of course, it could be structured in an easier/neater way,
-# and if a student decides to commit to that, that is OK.
-
-# NOTE OF ADVICE:
-# When you go to make your GitHub milestones, think pretty seriously
-# about all the different parts and their requirements,
-# and what you need to understand.
-# Make sure you've asked your questions about Part 2 as much as you need to
-# before Fall Break!
-
-
-# PART 0
-
-try:
-    gallery_file = open("gallery.html", "r")
-    html = gallery_file.read()
-    gallery_file.close()
-except:
-    newman_taylor_gallery = "http://newmantaylor.com/gallery.html"
-    response = requests.get(newman_taylor_gallery)
-    html = response.text
-    gallery_file = open("gallery.html", "w")
-    gallery_file.write(html)
-    gallery_file.close()
-
-gallery_soup = BeautifulSoup(html, "html.parser")
-img_alt_text_lst = []
-imgs = gallery_soup.find_all("img")
-for img in imgs:
-    alt = img.get("alt", "No alternative text provided!")
-    img_alt_text_lst.append(alt)
-for element in img_alt_text_lst:
-    print(element)
-
 # PART 1
 
 
@@ -46,6 +10,9 @@ def get_nps_homepage_and_state_html(state_str):
         nps_gov_file = open("nps_gov_data.html", "r")
         html = nps_gov_file.read()
         nps_gov_file.close()
+        print1 = "NPS homepage HTML file already exists."
+        print2 = "  (This isn't a fruitful function.)"
+        print(print1 + print2)
     except:
         nps_gov_home = "https://www.nps.gov/index.htm"
         response = requests.get(nps_gov_home)
@@ -53,7 +20,9 @@ def get_nps_homepage_and_state_html(state_str):
         nps_gov_file = open("nps_gov_data.html", "w")
         nps_gov_file.write(html)
         nps_gov_file.close()
-
+        print3 = "NPS homepage HTML file written."
+        print4 = "  (This isn't a fruitful function.)"
+        print(print3 + print4)
     nps_gov_soup = BeautifulSoup(html, "html.parser")
     state_str_lc = state_str.lower()
     state_str_lc_no_spaces = state_str_lc.replace(" ", "_")
@@ -63,28 +32,26 @@ def get_nps_homepage_and_state_html(state_str):
         state_data_file = open(state_data_html, "r")
         html = state_data_file.read()
         state_data_file.close()
+        print5 = "NPS state HTML file already exists."
+        print6 = "  (This isn't a fruitful function.)"
+        print(print5 + print6)
     except:
         dropdown_class = "dropdown-menu SearchBar-keywordSearch"
         dropdown = nps_gov_soup.find("ul", {"class": dropdown_class})
-        for dropdown_li in dropdown.children:
-            try:
-                state_link = dropdown_li.contents[0]
-                state_link_text = state_link.text
-                if state_link_text == state_str:
-                    state_link_href = state_link.get("href")
-                    nps_gov_prefix = "https://www.nps.gov"
-                    nps_state = nps_gov_prefix + state_link_href
-                    response = requests.get(nps_state)
-                    html = response.text
-                    nps_state_file = open(state_data_html, "w")
-                    nps_state_file.write(html)
-                    nps_state_file.close()
-            except:
-                pass
-
-    print1 = "Not a fruitful function, but your NPS homepage HTML file "
-    print2 = "and your NPS state HTML file are ready.\n"
-    print(print1 + print2)
+        for state_li in dropdown.find_all("li"):
+            state_link_text = state_li.find("a").text
+            if state_link_text == state_str:
+                state_link_href = state_li.find("a").get("href")
+                nps_gov_prefix = "https://www.nps.gov"
+                nps_state = nps_gov_prefix + state_link_href
+                response = requests.get(nps_state)
+                html = response.text
+                nps_state_file = open(state_data_html, "w")
+                nps_state_file.write(html)
+                nps_state_file.close()
+        print7 = "NPS state HTML file written."
+        print8 = "  (This isn't a fruitful function.)"
+        print(print7 + print8)
 
 get_nps_homepage_and_state_html("Arkansas")
 get_nps_homepage_and_state_html("California")
@@ -125,7 +92,7 @@ class NationalSite(object):
         site_name_lower_underscore = site_name_lower.replace(" ", "_")
         basic_info_html_suffix = "_basic_info.html"
         html_name = site_name_lower_underscore + basic_info_html_suffix
-        try:  # SHOULD REWRITE TO CACHE IN ONE JSON FILE, NOT MANY HTML FILES
+        try:  # SHOULD'VE CACHED IN ONE JSON FILE, NOT MANY HTML FILES
             site_basic_info_file = open(html_name, "r")
             html = site_basic_info_file.read()
             site_basic_info_file.close()
@@ -136,9 +103,9 @@ class NationalSite(object):
             site_basic_info_file.write(html)
             site_basic_info_file.close()
         info_soup = BeautifulSoup(html, "html.parser")
+        address_str = ""
         try:
             address = info_soup.find("div", {"itemprop": "address"})
-            address_str = ""
             address_lst = []
             address_children = address.contents
             for address_child in address_children:
@@ -151,9 +118,19 @@ class NationalSite(object):
             for address_element in address_lst[:-1]:
                 address_str = address_str + address_element + " / "
             address_str = address_str + address_lst[-1]
-            return address_str
         except:
-            return ""
+            pass
+        return address_str
+
+        # A cleaner way of accomplishing lines 109-120
+        # (using .find_all() instead of .contents) is below,
+        # but the addresses that it generates have repeats,
+        # while those generated by lines 109-120 do not:
+
+            # address_spans = address.find_all("span")
+            # for address_span in address_spans[:-1]:
+            #    address_str = address_str + address_span.text.strip() + " / "
+            # address_str = address_str + address_spans[-1].text.strip()
 
 # PART 3
 
@@ -166,7 +143,6 @@ def create_state_nationalsites_list(state_str):
     state_data_file = open(state_data_html, "r")
     html = state_data_file.read()
     state_data_file.close()
-
     state_sites_list = []
     state_soup = BeautifulSoup(html, "html.parser")
     state_sites_ul = state_soup.find("ul", {"id": "list_parks"})
@@ -186,33 +162,39 @@ michigan_natl_sites = create_state_nationalsites_list("Michigan")
 
 
 def write_site_objects_list_to_csv(list_of_site_objects, file_name):
-    outfile = open(file_name, "w")
-    outfile.write("NAME, LOCATION, TYPE, ADDRESS, DESCRIPTION\n")
-    for site_object in list_of_site_objects:
-        site_name = site_object.name
-        if site_object.location:
-            site_location = site_object.location
-        else:
-            site_location != ""
-        if site_object.type:
-            site_type = site_object.type
-        else:
-            site_type = "None"
-        if site_object.get_mailing_address() != "":
-            site_address = site_object.get_mailing_address()
-        else:
-            site_address = "None"
-        if site_object.description:
-            site_description = site_object.description.replace('"', '``')
-        else:
-            site_description = "None"
-        outfile.write('"{}",'.format(site_name))
-        outfile.write('"{}",'.format(site_location))
-        outfile.write('"{}",'.format(site_type))
-        outfile.write('"{}",'.format(site_address))
-        outfile.write('"{}"'.format(site_description))
-        outfile.write("\n")
-    outfile.close()
+    try:
+        infile = open(file_name, "r")
+        infile.close()
+        print("CSV file already exists.  (This isn't a fruitful function.)")
+    except:
+        outfile = open(file_name, "w")
+        outfile.write("NAME, LOCATION, TYPE, ADDRESS, DESCRIPTION\n")
+        for site_object in list_of_site_objects:
+            site_name = site_object.name
+            if site_object.location:
+                site_location = site_object.location
+            else:
+                site_location != ""
+            if site_object.type:
+                site_type = site_object.type
+            else:
+                site_type = "None"
+            if site_object.get_mailing_address() != "":
+                site_address = site_object.get_mailing_address()
+            else:
+                site_address = "None"
+            if site_object.description:
+                site_description = site_object.description.replace('"', "''")
+            else:
+                site_description = "None"
+            outfile.write('"{}",'.format(site_name))
+            outfile.write('"{}",'.format(site_location))
+            outfile.write('"{}",'.format(site_type))
+            outfile.write('"{}",'.format(site_address))
+            outfile.write('"{}"'.format(site_description))
+            outfile.write("\n")
+        outfile.close()
+        print("CSV file written.  (This isn't a fruitful function.)")
 
 csv_ar = "arkansas.csv"
 csv_ca = "california.csv"
